3f8bf964ccb7b1831d4ea625fed734ca4c7a3871
Krista Loven <krista.loven.87@gmail.com>
2019-11-04 21:26:33 -0800

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -6,4 +6,39 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends Component {
+  state = {
+    reviews: []
+  }
+
+  componentDidMount() {
+    this.fetchReviews()
+  }
+
+  fetchReviews = () => {
+    fetch(URL)
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({reviews: data.results})
+      })
+  }
+
+  parseReviews = () => {
+    return this.state.reviews.map((movie, idx) => {
+      return (
+        <div className="review-list" key={idx}>
+          <MovieReviews movie={movie} />
+        </div>
+      )
+    })
+  }
+
+  render () {
+    return (
+    <div className='latest-movie-reviews'>
+      <h1>Latest Movie Reviews:</h1>
+      {this.parseReviews()}
+    </div>
+    )
+  }
+}
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,4 +6,58 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+  state = {
+    searchTerm: '',
+    reviews: []
+  }
+
+  fetchMovies = () => {
+    fetch(URL + `&query=${this.state.searchTerm}`).then(resp => resp.json())
+      .then(data => this.setState({
+        reviews: data.results
+      })
+    )
+  }
+
+  parseReviews = () => {
+    return this.state.reviews.map((movie, idx) => {
+      return (
+        <div className="review-list" key={idx}>
+          <MovieReviews movie={movie} />
+        </div>
+      )
+    })
+  }
+
+  componentDidMount() {
+    this.fetchMovies()
+  }
+
+  handleReviewSearch = (event) => {
+    event.preventDefault()
+    this.setState({
+      searchTerm: this.state.searchTerm
+    }, () => this.fetchMovies())
+  }
+
+  handleTyping = (event) => {
+    event.preventDefault()
+    this.setState({
+      searchTerm: event.target.value
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Search for Movie Reviews:</h1>
+        <form onSubmit={this.handleReviewSearch}>
+          <input type="text" value={this.state.searchTerm} onChange={this.handleTyping} />
+          <button type="submit">Search</button>
+        </form>
+        {this.parseReviews()}
+      </div>
+    )
+  }
+}
